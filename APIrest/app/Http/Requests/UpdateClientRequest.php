c1884c5da622b508dd937190557933e3d72d731c
Jaki Riggs <jarquealex0302@gmail.com>
2026-01-25 11:31:28 +0100

    adding everything

---
+++ APIrest/app/Http/Requests/UpdateClientRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateClientRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string'],
+            'email' => ['sometimes', 'email'],
+            'phone' => ['sometimes', 'string'],
+            'address' => ['sometimes', 'string'],
+        ];
+    }
+}
