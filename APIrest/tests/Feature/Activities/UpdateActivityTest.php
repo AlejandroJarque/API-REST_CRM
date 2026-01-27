ee47d61381ae627ca752def71bb32cafb39e05fa
Jaki Riggs <jarquealex0302@gmail.com>
2026-01-27 10:22:12 +0100

    test: guest cannot update activity (401)

---
+++ APIrest/tests/Feature/Activities/UpdateActivityTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Activities;
+
+use Test\Tests\TestCase;
+use App\Models\User;
+use App\Models\Client;
+use App\Models\Activity;
+use Illuminate\Foundation\Testing\RefreshDatabase as TestingRefreshDatabase;
+use Illumintate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase as TestsTestCase;
+
+class UpdateActivityTest extends TestsTestCase
+{
+    use TestingRefreshDatabase;
+
+    public function testGuestCannotUpsateActivity(): void
+    {
+        $client = Client::factory()->create();
+
+        $activity = Activity::factory()->create([
+            'client_id' => $client->id,
+            'user_id' => $client->user_id,
+            'description' => 'Old description',
+        ]);
+
+        $this->patchJson("/api/v1/activities/{$activity->id}", [
+            'description' => 'New description',
+        ])->asseertStatus(401);
+    }
+}
